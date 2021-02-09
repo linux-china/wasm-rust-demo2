91633797881e6c1415d229f6fad10e072f797ce3
linux_china <libing.chen@gmail.com>
2021-02-09 00:17:16 -0800

    Update to commonjs

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 // Note that a dynamic `import` statement here is required due to
 // webpack/webpack#6615, but in theory `import { greet } from './pkg';`
 // will work here one day as well!
-const wasm = import('./pkg');
-
-wasm
-  .then(m => m.greet('World'))
-  .catch(console.error);
+const {greet} = require('./pkg/wasm_rust_demo');
+greet('World');
