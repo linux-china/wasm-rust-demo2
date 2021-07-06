8362899697d08e4cfcb10e0ed4431dcea92af074
linux_china <libing.chen@gmail.com>
2021-07-05 17:01:47 -0700

    Polishing

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// Note that a dynamic `import` statement here is required due to
-// webpack/webpack#6615, but in theory `import { greet } from './pkg';`
-// will work here one day as well!
-const {greet} = require('./pkg/wasm_rust_demo');
+import {greet} from "./pkg";
+
 greet('World');
